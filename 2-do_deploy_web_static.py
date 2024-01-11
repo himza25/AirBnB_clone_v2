e0c2152d4b193d882e0bccaea42d89c52e416fef
himza25 <himza.of.theking95@gmail.com>
2024-01-11 15:02:44 -0800

    task 2 static deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,54 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script that distributes an archive to your web servers.
 """
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['3.85.168.165', '100.24.72.68']
+
+env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """ Distributes an archive to the web servers. """
+    if not exists(archive_path):
+        print("Archive path does not exist")
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        file_name = archive_path.split("/")[-1]
+        name_no_ext = file_name.split(".")[0]
+        remote_tmp_path = "/tmp/{}".format(file_name)
+        remote_release_folder = "/data/web_static/releases/{}/".format(
+            name_no_ext)
+
+        # Upload the archive
+        print("Uploading the archive...")
+        put(archive_path, remote_tmp_path)
+        print("Archive uploaded.")
+
+        # Operations on the server
+        print("Creating release directory...")
+        run("mkdir -p {}".format(remote_release_folder))
+        print("Release directory created.")
+
+        print("Uncompressing the archive...")
+        run("tar -xzf {} -C {}".format(remote_tmp_path, remote_release_folder))
+        print("Archive uncompressed.")
+
+        print("Moving content out of web_static directory...")
+        run("mv {}web_static/* {}".format(remote_release_folder,
+                                          remote_release_folder))
+        print("Content moved.")
+
+        print("Cleaning up...")
+        run("rm -rf {}web_static".format(remote_release_folder))
+        run("rm {}".format(remote_tmp_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(remote_release_folder))
+        print("New version deployed.")
+
         return True
-    except:
+    except Exception as e:
+        print("An error occurred: {}".format(e))
         return False
