999b86d288afc868bd89179fe0ceeadf9e71d277
himza25 <himza.of.theking95@gmail.com>
2024-01-11 19:19:50 -0800

    task 5 deploys static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,57 +1,68 @@
 # 101-setup_web_static.pp
 class web_static_setup {
+    # Ensure Nginx is installed
     package { 'nginx':
         ensure => installed,
     }
 
+    # Ensure directories are present and have correct owner and group
+    file { '/data':
+        ensure => directory,
+        owner  => 'ubuntu',
+        group  => 'ubuntu',
+    }
+
     file { '/data/web_static':
-        ensure => 'directory',
+        ensure => directory,
         owner  => 'ubuntu',
         group  => 'ubuntu',
     }
 
     file { '/data/web_static/releases':
-        ensure => 'directory',
+        ensure => directory,
         owner  => 'ubuntu',
         group  => 'ubuntu',
     }
 
     file { '/data/web_static/shared':
-        ensure => 'directory',
+        ensure => directory,
         owner  => 'ubuntu',
         group  => 'ubuntu',
     }
 
     file { '/data/web_static/releases/test':
-        ensure => 'directory',
+        ensure => directory,
         owner  => 'ubuntu',
         group  => 'ubuntu',
     }
 
+    # Create a fake HTML file
     file { '/data/web_static/releases/test/index.html':
-        ensure  => 'present',
-        content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n",
+        ensure  => present,
+        content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n',
         owner   => 'ubuntu',
         group   => 'ubuntu',
     }
 
+    # Create a symbolic link
     file { '/data/web_static/current':
-        ensure => 'link',
+        ensure => link,
         target => '/data/web_static/releases/test',
         force  => true,
         require => File['/data/web_static/releases/test/index.html'],
     }
 
+    # Update Nginx configuration
     exec { 'update_nginx_config':
         command => "sed -i '/server_name _;/a \\\n\\tlocation /hbnb_static/ {\\n\\t\\talias /data/web_static/current/;\\n\\t}\\n' /etc/nginx/sites-available/default",
-        refreshonly => true,
-        subscribe => File['/data/web_static/current'],
+        onlyif  => "grep -q 'server_name _;' /etc/nginx/sites-available/default",
+        notify  => Service['nginx'],
     }
 
+    # Ensure the Nginx service is running and enabled
     service { 'nginx':
         ensure => running,
         enable => true,
-        require => Exec['update_nginx_config'],
     }
 }
 
