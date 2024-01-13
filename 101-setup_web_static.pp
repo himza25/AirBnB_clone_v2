436cc7ff37245dac83de9d763dfef8ed97a7f704
himza25 <himza.of.theking95@gmail.com>
2024-01-13 05:19:59 -0800

    task 5 deploy static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,34 +1,45 @@
-# /path/to/101-setup_web_static.pp
-# Puppet manifest to set up web servers for deployment of web_static
+# 101-setup_web_static.pp
 
 # Ensure Nginx is installed
 package { 'nginx':
   ensure => installed,
 }
 
-# Create necessary directories
-file { [
-    '/data',
-    '/data/web_static',
-    '/data/web_static/releases',
-    '/data/web_static/shared',
-    '/data/web_static/releases/test',
-  ]:
-  ensure => directory,
+# Create required directories
+file { '/data':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu',
 }
 
 # Create a fake HTML file
 file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>',
+  ensure  => 'present',
+  content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n',
   owner   => 'ubuntu',
   group   => 'ubuntu',
 }
@@ -37,28 +48,7 @@ file { '/data/web_static/releases/test/index.html':
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test',
+  force  => true,
   owner  => 'ubuntu',
   group  => 'ubuntu',
-  force  => true,
-}
-
-# Configure Nginx
-exec { 'nginx-config':
-  command => 'ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/',
-  unless  => 'test -L /etc/nginx/sites-enabled/default',
-  notify  => Service['nginx'],
-}
-
-file_line { 'nginx-server-block':
-  path    => '/etc/nginx/sites-available/default',
-  line    => "\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n",
-  match   => '^server_name _;',
-  append_on_no_match => true,
-  notify  => Service['nginx'],
-}
-
-# Ensure Nginx is running and enabled
-service { 'nginx':
-  ensure => running,
-  enable => true,
 }
