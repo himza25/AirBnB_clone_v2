33a50a44be02b0793ca89ec561ab81a0fa7955df
himza25 <himza.of.theking95@gmail.com>
2024-01-13 06:13:05 -0800

    Update task 4 deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-"""
-Fabric script to delete out-of-date archives.
-"""
+# Fabfile to delete out-of-date archives.
+import os
+from fabric.api import *
 
-from fabric.api import local, sudo, env
-
-env.hosts = ['3.85.168.165', '100.24.72.68']
-env.user = "ubuntu"
+env.hosts = ["3.85.168.165", "100.24.72.68"]
 
 def do_clean(number=0):
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
     """
-    Deletes out-of-date archives.
-    """
-    number = int(number)
+    number = 1 if int(number) == 0 else int(number)
 
-    if number == 0:
-        number = 1
+    # Clean local archives
+    local_archives = sorted(os.listdir("versions"))
+    local_archives = [a for a in local_archives if "web_static_" in a]
+    archives_to_delete = local_archives[:-number]
 
-    # Delete local archives
-    local("ls -tr versions/web_static_*.tgz | head -n -{} | xargs rm -f".format(number))
+    with lcd("versions"):
+        for archive in archives_to_delete:
+            local("rm ./{}".format(archive))
 
-    # Delete remote archives
-    sudo("ls -tr /data/web_static/releases/web_static_* | head -n -{} | xargs rm -rf".format(number))
+    # Clean archives on remote servers
+    with cd("/data/web_static/releases"):
+        remote_archives = run("ls -tr").split()
+        remote_archives = [a for a in remote_archives if "web_static_" in a]
+        remote_archives_to_delete = remote_archives[:-number]
 
-if __name__ == "__main__":
-    do_clean()
+        for archive in remote_archives_to_delete:
+            run("rm -rf ./{}".format(archive))
