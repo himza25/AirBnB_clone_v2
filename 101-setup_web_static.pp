28b81440244e9c645f0f0063be7121391874c451
himza25 <himza.of.theking95@gmail.com>
2024-01-13 05:53:25 -0800

    task 5 deploy static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,15 +1,37 @@
-# 101-setup_web_static.pp
+# Configures a web server for deployment of web_static.
+
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
 # Ensure Nginx is installed
 package { 'nginx':
-  ensure => installed,
+  ensure   => 'present',
+  provider => 'apt',
 }
 
-# Create required directories
+# Create the directory structure
 file { '/data':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
 }
 
 file { '/data/web_static':
@@ -24,31 +46,62 @@ file { '/data/web_static/releases':
   group  => 'ubuntu',
 }
 
-file { '/data/web_static/shared':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
 file { '/data/web_static/releases/test':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu',
 }
 
-# Create a fake HTML file
+file { '/data/web_static/shared':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+# Create the test HTML file
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n',
+  content => "Holberton School Puppet\n",
   owner   => 'ubuntu',
   group   => 'ubuntu',
 }
 
-# Create a symbolic link
+# Create the symbolic link
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test',
-  force  => true,
   owner  => 'ubuntu',
   group  => 'ubuntu',
+}
+
+# Nginx configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf,
+  notify  => Exec['nginx_restart'],
+}
+
+# Restart Nginx to apply changes
+exec { 'nginx_restart':
+  command     => '/etc/init.d/nginx restart',
+  refreshonly => true,
+}
+
+# Ensure /var/www and its subdirectories exist
+file { '/var/www':
+  ensure => 'directory',
+}
+
+file { '/var/www/html':
+  ensure => 'directory',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n",
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n",
 }
