e5d96635da91ba095b65533b865294d3047986de
himza25 <himza.of.theking95@gmail.com>
2024-01-26 01:10:31 -0800

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,54 +1,51 @@
 #!/usr/bin/python3
-"""run flask server"""
+"""
+A Flask web application module.
+This module starts a Flask web application that listens on 0.0.0.0, port 5000.
+It defines routes '/', '/hbnb', '/c/<text>', '/python/(<text>)', '/number/<n>',
+and '/number_template/<n>'.
+'/' route displays "Hello HBNB!", '/hbnb' route displays "HBNB",
+'/c/<text>' displays "C " followed by the value of the text variable,
+'/python/(<text>)' displays "Python ",
+followed by the value of the text variable,
+'/number/<n>' displays "n is a number" only if n is an integer,
+'/number_template/<n>' renders an HTML template only if n is an integer.
+"""
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def makeSpaces(text):
-    string = ""
-    for i in text:
-        if i == '_':
-            string += ' '
-        else:
-            string += i
-    return string
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """hello returned"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb returned"""
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def cIsFun(text):
-    """hbnb returned"""
-    return "C {}".format(makeSpaces(text))
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pyIsFun(text="is cool"):
-    """hbnb returned"""
-    return "Python {}".format(makeSpaces(text))
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def integer(n):
-    """hbnb returned"""
-    return "{} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def integer_tmp(n):
-    """hbnb returned"""
-    return render_template('5-number.html', n=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Renders an HTML template only if n is an integer. """
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == "__main__":
