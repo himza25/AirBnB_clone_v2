05a79d8347f8e45678d50c95ba795c8d93398d24
himza25 <himza.of.theking95@gmail.com>
2024-01-26 01:04:40 -0800

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,55 +1,55 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+"""run flask server"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    """Return string when route queried
-    """
-    return 'Hello HBNB!'
+def makeSpaces(text):
+    string = ""
+    for i in text:
+        if i == '_':
+            string += ' '
+        else:
+            string += i
+    return string
 
 
-@app.route('/hbnb')
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """hello returned"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Return string when route queried
-    """
-    return 'HBNB'
+    """hbnb returned"""
+    return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text
-    """
-    return 'C ' + text.replace('_', ' ')
+@app.route("/c/<text>", strict_slashes=False)
+def cIsFun(text):
+    """hbnb returned"""
+    return "C {}".format(makeSpaces(text))
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    """
-    return 'Python ' + text.replace('_', ' ')
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pyIsFun(text="is cool"):
+    """hbnb returned"""
+    return "Python {}".format(makeSpaces(text))
 
 
-@app.route('/number/<int:n>')
-def number(n=None):
-    """Allow request if path variable is a valid integer
-    """
-    return str(n) + ' is a number'
+@app.route("/number/<int:n>", strict_slashes=False)
+def integer(n):
+    """hbnb returned"""
+    return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>')
-def number_template(n):
-    """Retrieve template for request
-    """
-    path = '5-number.html'
-    return render_template(path, n=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def integer_tmp(n):
+    """hbnb returned"""
+    return render_template('5-number.html', n=n)
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
