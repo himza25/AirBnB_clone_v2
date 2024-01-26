13e3b90b13f708061c5a348f354507fcd81f125f
himza25 <himza.of.theking95@gmail.com>
2024-01-26 06:52:19 -0800

    task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""run flask server"""
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states():
-    """states returned"""
-    return render_template('9-states.html\
-', states=storage.all("State"), state=None)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def id_state(id):
-    """states returned"""
-    states = storage.all("State")
-    # if "State.{}".format(id) not in states:
-    #     return render_template('9-states.html')
-    state = storage.all("State").get("State.{}".format(id))
-    return render_template('9-states.html\
-', state=state, states=None)
-
-
 @app.teardown_appcontext
-def reset(error):
-    """reload data"""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/states', strict_slashes=False)
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_by_id(id):
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
