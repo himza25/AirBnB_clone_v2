c0f60eb855af4719b060ba004df84aa733c69643
himza25 <himza.of.theking95@gmail.com>
2024-01-26 06:21:21 -0800

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,39 +1,55 @@
 #!/usr/bin/python3
 """
-Flask web application that displays cities by states from the storage engine.
+This Flask web application provides two routes:
+one for listing states and another for listing cities by states.
 """
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
+def sortdict(dictionary):
     """
-    Renders a HTML page listing all States and their Cities,
-    sorted alphabetically.
+    Sorts and returns a dictionary.
     """
-    states = storage.all(State).values()
+    diction = {}
+    for i in dictionary:
+        diction[i] = dictionary[i]
+    return diction
+
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """
+    Route to display a list of states. Renders the states_list template.
+    """
+    return render_template('7-states_list.html', states=storage.all("State"))
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_state():
+    """
+    Route to display a list of cities by states.
+    Renders the cities_by_states template.
+    """
+    states = storage.all("State")
     cities_dict = {}
-    for state in states:
-        cities_dict[state.name] = sorted(state.cities,
-                                         key=lambda city: city.name)
-    # Sort states alphabetically by name
-    sorted_states = sorted(states, key=lambda state: state.name)
+    for state in states.values():
+        cities_dict[state.name] = sorted(
+                                state.cities, key=lambda city: city.name
+                                )
     return render_template('8-cities_by_states.html',
-                           states=sorted_states, cities=cities_dict)
+                           states=states, cities=cities_dict)
 
 
 @app.teardown_appcontext
-def close_session(exception):
+def reset(error):
     """
-    Closes the SQLAlchemy session after each request.
+    Clean-up method to close the storage connection after each request.
     """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
