103cf956a1822e04dc456ae083c8142f34503417
himza25 <himza.of.theking95@gmail.com>
2024-01-26 06:35:01 -0800

    task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,7 +4,7 @@ This module starts a Flask web application to display states and cities.
 """
 
 from flask import Flask, render_template
-from models import storage
+from models import storage, State
 
 app = Flask(__name__)
 
@@ -12,19 +12,19 @@ app = Flask(__name__)
 @app.route('/states', strict_slashes=False)
 def states_list():
     """ Display a HTML page with a list of all states. """
-    states = storage.all("State").values()
-    return render_template('9-states.html', states=states)
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states, state=None)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def state_cities(id):
-    """ Display a HTML page with a list of cities of a specific state. """
+    """ Display a HTML page with cities of a specific state. """
     state = None
-    for st in storage.all("State").values():
+    for st in storage.all(State).values():
         if st.id == id:
             state = st
             break
-    return render_template('9-states.html', state=state)
+    return render_template('9-states.html', state=state, states=None)
 
 
 @app.teardown_appcontext
