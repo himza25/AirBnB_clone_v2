118b83bd98a137c1ad8d9dd44f055c675786ccca
himza25 <himza.of.theking95@gmail.com>
2024-01-26 00:26:40 -0800

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A Flask web application module.
+This module starts a Flask web application that listens on 0.0.0.0, port 5000.
+It defines routes '/', '/hbnb', '/c/<text>', '/python/(<text>)',
+and '/number/<n>'.
+'/' route displays "Hello HBNB!", '/hbnb' route displays "HBNB",
+'/c/<text>' displays "C " followed by the value of the text variable,
+'/python/(<text>)' displays "Python ", followed by the value of the text.
+'/number/<n>' displays "n is a number" only if n is an integer.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ Displays 'n is a number' only if n is an integer. """
+    return f'{n} is a number'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
