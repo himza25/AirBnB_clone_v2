99391e71e6adcb54eff7f5234a40ca5ac0ccffa8
himza25 <himza.of.theking95@gmail.com>
2024-01-26 05:08:03 -0800

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """Start a Flask web application."""
 from flask import Flask, render_template
-from models import storage
+from models import storage, State
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a HTML page with a list of states and cities."""
+    """Display a HTML page with a list of states and cities sorted by name."""
     states = storage.all("State").values()
-    states = sorted(states, key=lambda s: s.name)
-    return render_template('8-cities_by_states.html', states=states)
+    states_cities = {}
+    for state in sorted(states, key=lambda s: s.name):
+        states_cities[state] = sorted(state.cities, key=lambda c: c.name)
+    return render_template('8-cities_by_states.html',
+                           states_cities=states_cities)
 
 
 @app.teardown_appcontext
