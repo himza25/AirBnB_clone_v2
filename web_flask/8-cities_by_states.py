0609ecad1fb5aa40d9a301c21dd004d60e2c7b10
himza25 <himza.of.theking95@gmail.com>
2024-01-26 05:35:05 -0800

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
-"""
-Flask web application that displays cities by states from the storage engine.
-"""
+"""Start web application with routes for states and cities by states."""
 
-from flask import Flask, render_template
 from models import storage
-
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route('/states_list')
+def states_list():
+    """Render template with states."""
+    states = storage.all(State)
+    # Sort State objects alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', sorted_states=sorted_states)
+
+
+@app.route('/cities_by_states')
 def cities_by_states():
-    """
-    Renders a HTML page listing all States and their Cities.
-    """
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+    """Render template with states and their cities."""
+    states = storage.all(State)
+    # Sort State objects alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """
-    Closes the SQLAlchemy session after each request.
-    """
+def app_teardown(arg=None):
+    """Clean-up session."""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
